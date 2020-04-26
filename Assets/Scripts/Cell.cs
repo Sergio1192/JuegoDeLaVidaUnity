1cacbec21f6b173f788c637a1b3492a4d785e170
Sergio1192 <sergio11_92@hotmail.com>
2020-04-26 20:18:37 +0100

    Grid completo con evento de raton

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+    public Material DeadMaterial;
+    public Material AliveMaterial;
+
+    private bool _isAlive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Dead();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Dead()
+    {
+        _isAlive = false;
+
+        var meshRenderer = GetComponent<MeshRenderer>();
+        meshRenderer.material = DeadMaterial;
+    }
+
+    public void Alive()
+    {
+        _isAlive = true;
+
+        var meshRenderer = GetComponent<MeshRenderer>();
+        meshRenderer.material = AliveMaterial;
+    }
+
+    private void OnMouseDown()
+    {
+        if (_isAlive)
+            Dead();
+        else
+            Alive();
+    }
+}
---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : MonoBehaviour
+{
+    public GameObject CellPrefab;
+
+    private const int NUMBER_COLUMNS = 100;
+    private const int NUMBER_ROWS = 100;
+
+    private Dictionary<Vector2, Cell> _cells = new Dictionary<Vector2, Cell>();
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateGrid();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CreateGrid()
+    {
+        for (int y = 0; y < NUMBER_ROWS; y++)
+        {
+            for (int x = 0; x < NUMBER_COLUMNS; x++)
+            {
+                var position = new Vector2(x, y);
+                var cell = Instantiate(CellPrefab, position, Quaternion.identity, transform);
+
+                _cells.Add(position, cell.GetComponent<Cell>());
+            }
+        }
+    }
+
+
+}
